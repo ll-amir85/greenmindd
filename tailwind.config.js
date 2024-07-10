d754a3185c762fec171b64d0abff267bdf550564
pouya <pouya@gmail.com>
2024-07-10 18:24:31 +0330

    fix : custom color and add container & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors : {
+      "primary" : "#C1DCDC",
+      "white": "#ffffff",
+      "black":"#1E1E1E",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2.5rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
   },
   plugins: [],
 }
